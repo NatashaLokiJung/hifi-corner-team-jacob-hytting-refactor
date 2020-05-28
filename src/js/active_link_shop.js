72797d92b510f1255af9853df92cd5daa179e3e9
NatashaLokiJung <427370@rts-365.dk>
2020-05-28 13:59:44 +0200

    Active links er udkommenteret

--- src/js/active_link_shop.js
+++ src/js/active_link_shop.js
@@ -8,11 +8,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let menuItem = document.querySelector(".navigation").getElementsByClassName("navigation__link");
     let menuLength = menuItem.length;
+    let shop = "kategoriliste";
+    let product = "/";
+
 
     for (let i = 0; i < menuLength; i++) {
       if (menuItem[i].getAttribute("href").indexOf(currentLocation) == 1) {
         menuItem[i].classList.add('navigation__link_active');
-      } else {
+      }else if(
+        menuItem[i].getAttribute("href").indexOf(shop) == 1 || menuItem[i].getAttribute("href").indexOf(product) == 1){
+        menuItem[i].classList.add('navigation__link_active')
+      }else {
         menuItem[i].classList.remove('navigation__link_active');
       }
     }
@@ -20,10 +26,5 @@ document.addEventListener("DOMContentLoaded", function () {
 
   setNavigation()
 
-
-
-
-
-
-
+  //  netilify video https://www.youtube.com/watch?v=rs690lwkZ2E&list=PL14RtI8hHAjTGmSVsR-MpV2hReVNMqbEY&index=16
 });
--- tmp/assets/js/active_link_shop.js
+++ tmp/assets/js/active_link_shop.js
@@ -8,11 +8,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let menuItem = document.querySelector(".navigation").getElementsByClassName("navigation__link");
     let menuLength = menuItem.length;
+    let shop = "kategoriliste";
+    let product = "/";
+
 
     for (let i = 0; i < menuLength; i++) {
       if (menuItem[i].getAttribute("href").indexOf(currentLocation) == 1) {
         menuItem[i].classList.add('navigation__link_active');
-      } else {
+      }else if(
+        menuItem[i].getAttribute("href").indexOf(shop) == 1 || menuItem[i].getAttribute("href").indexOf(product) == 1){
+        menuItem[i].classList.add('navigation__link_active')
+      }else {
         menuItem[i].classList.remove('navigation__link_active');
       }
     }
@@ -20,11 +26,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
   setNavigation()
 
-
-
-
-
-
-
+  //  netilify video https://www.youtube.com/watch?v=rs690lwkZ2E&list=PL14RtI8hHAjTGmSVsR-MpV2hReVNMqbEY&index=16
 });
 //# sourceMappingURL=active_link_shop.js.map
